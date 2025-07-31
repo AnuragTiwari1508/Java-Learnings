50c29b349f2f2cdfb80c023b7609ac66411af5b6
anurag tiwari <168359424+AnuragTiwari1508@users.noreply.github.com>
2025-07-31 11:58:42 +0530

    Initial commit with Java tutorials and batch script

---
+++ tut2.java
@@ -0,0 +1,11 @@
+public class tut2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming!");
+        System.out.println("This is a simple program.");
+        System.out.print("Please enter your name: ");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Thank you for using this program.");
+    }
+}
